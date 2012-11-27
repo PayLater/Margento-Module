80efd302c5b0575e45d5938b207666602f7d2eca
Carlo Tasca <carlo.tasca.mail@gmail.com>
2012-11-27 14:47:53 +0000

    initial module config, system.xml and admin template

---
+++ app/code/local/Wonga/PayLater/Block/Adminhtml/System/Config/Fieldset/Info.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Wonga.com Ltd extension for Magento
+ *
+ * Long description of this file (if any...)
+ *
+ * NOTICE OF LICENSE
+ *
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade
+ * the Wonga PayLater module to newer versions in the future.
+ * If you wish to customize the Wonga PayLater module for your needs
+ * please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category   Wonga
+ * @package    Wonga_PayLater
+ * @copyright  Copyright (C) 2012 Wonga.com Ltd (http://www.wonga.com/)
+ * @license    
+ */
+
+/**
+ * 
+ *
+ * @category   Wonga
+ * @package    Wonga_PayLater
+ * @subpackage Block
+ * @author     GPMD Ltd <dev@gpmd.co.uk>
+ */
+class Wonga_PayLater_Block_Adminhtml_System_Config_Fieldset_Info extends Mage_Adminhtml_Block_Abstract implements Varien_Data_Form_Element_Renderer_Interface
+{
+	protected $_template = 'checkoutsuite/onepage/system/config/fieldset/info.phtml';
+	
+	/**
+     * Render fieldset html
+     *
+     * @param Varien_Data_Form_Element_Abstract $element
+     * @return string
+     */
+    public function render(Varien_Data_Form_Element_Abstract $element)
+    {
+        return $this->toHtml();
+    }
+	
+	public function getVersion()
+    {
+    	return (string) Mage::getConfig()->getNode('modules/Wonga_PayLater/version');
+    }
+	
+	public function getLicenseUrl()
+	{
+		return Mage::getConfig()->getNode('license/url');
+	}
+	
+	public function getSupportEmail()
+	{
+		return Mage::getConfig()->getNode('support/email');
+	}
+	
+	
+	public function getReportEmail()
+	{
+		return Mage::getConfig()->getNode('report/bugs');
+	}
+	
+	public function getDevEmail()
+	{
+		return Mage::getConfig()->getNode('dev/email');
+	}
+
+}
---
+++ app/code/local/Wonga/PayLater/Model/Interface/PayLater.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Wonga.com Ltd extension for Magento
+ *
+ * Long description of this file (if any...)
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade
+ * the Wonga PayLater module to newer versions in the future.
+ * If you wish to customize the Wonga PayLater module for your needs
+ * please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category   Wonga
+ * @package    Wonga_PayLater
+ * @copyright  Copyright (C) 2012 Wonga.com Ltd (http://www.wonga.com/)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Short description of the class
+ *
+ * Long description of the class (if any...)
+ *
+ * @category   Wonga
+ * @package    Wonga_PayLater
+ * @subpackage Model
+ * @author     GPMD Ltd <dev@gpmd.co.uk>
+ */
+interface Wonga_PayLater_Model_Interface_Paylater
+{
+	const XML_NODE_SYSTEM_DEV_LOG_ACTIVE = 'dev/log/active';
+	const SYSTEM_CONFIG_INFO_TEMPLATE = 'wonga/paylater/system/config/fieldset/info.phtml';
+}

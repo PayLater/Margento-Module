3d37b26994da9906a1ec54a6bac13c26260b5d0a
Carlo Tasca <carlo.tasca.mail@gmail.com>
2012-12-16 13:14:24 +0000

    added event dispatchable interface and Module class

---
+++ app/code/local/PayLater/PayLater/Event/DispatchableInterface.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * PayLater extension for Magento
+ *
+ * Long description of this file (if any...)
+ *
+ * NOTICE OF LICENSE
+ *
+ * [TO BE DEFINED]
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade
+ * the Wonga PayLater module to newer versions in the future.
+ * If you wish to customize the PayLater module for your needs
+ * please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category   PayLater
+ * @package    PayLater_PayLater
+ * @copyright  Copyright (C) 2012 PayLater
+ * @license    [TO BE DEFINED]
+ */
+
+/**
+ *
+ * Mark a class as able to determine if a given amount is within PayLater range.
+ * 
+ * @category   PayLater
+ * @package    PayLater_PayLater
+ * @subpackage Helper
+ * @author     GPMD Ltd <dev@gpmd.co.uk>
+ */
+interface PayLater_PayLater_Event_DispatchableInterface
+{
+	public static function dispatchEvent($handle, array $parameters);
+}
---
+++ app/code/local/PayLater/PayLater/Module.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * PayLater extension for Magento
+ *
+ * Long description of this file (if any...)
+ *
+ * NOTICE OF LICENSE
+ *
+ * [TO BE DEFINED]
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade
+ * the Wonga PayLater module to newer versions in the future.
+ * If you wish to customize the PayLater module for your needs
+ * please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category   PayLater
+ * @package    PayLater_PayLater
+ * @copyright  Copyright (C) 2012 PayLater
+ * @license    [TO BE DEFINED]
+ */
+
+/**
+ *
+ * @category   PayLater
+ * @package    PayLater_PayLater
+ * @subpackage Helper
+ * @author     GPMD Ltd <dev@gpmd.co.uk>
+ */
+class PayLater_PayLater_Module implements PayLater_PayLater_Event_DispatchableInterface
+{
+
+	private function __construct()
+	{
+	}
+
+	public static function dispatchEvent($handle, array $parameters)
+	{
+		if (is_array($parameters) && count($parameters) > 0) {
+			$hanlde ? Mage::dispatchEvent($handle, $parameters) : NULL;
+		} else {
+			$hanlde ? Mage::dispatchEvent($handle) : NULL;
+		}
+	}
+
+}

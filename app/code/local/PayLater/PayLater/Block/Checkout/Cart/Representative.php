6e9fd45963b9d1f72b9a31fc03d955951ef20814
Carlo Tasca <carlo@gpmd.co.uk>
2013-05-31 11:21:46 +0100

    added paylater cart block frontend/backend

---
+++ app/code/local/PayLater/PayLater/Block/Checkout/Cart/Representative.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * PayLater extension for Magento
+ *
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * 
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade
+ * the PayLater PayLater module to newer versions in the future.
+ * If you wish to customize the PayLater PayLater module for your needs
+ * please contact PayLater.
+ *
+ * @category   PayLater
+ * @package    PayLater_PayLater
+ * @copyright  Copyright (C) 2013 PayLater
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ *
+ * @category   PayLater
+ * @package    PayLater_PayLater
+ * @subpackage Model
+ * @author     GPMD <dev@gpmd.co.uk>
+ */
+class PayLater_PayLater_Block_Checkout_Cart_Representative extends Mage_Checkout_Block_Cart_Abstract
+{
+	
+}

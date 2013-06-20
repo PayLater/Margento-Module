a1feee9333864ab2b3ba984ee61fe22b609bdc05
Carlo Tasca <carlo@gpmd.co.uk>
2013-06-20 09:33:30 +0100

    checkout widget init

--- app/code/local/PayLater/PayLater/Block/Payment/Form.php
+++ app/code/local/PayLater/PayLater/Block/Payment/Form.php
@@ -31,10 +31,16 @@
  * @subpackage Model
  * @author     GPMD <dev@gpmd.co.uk>
  */
-class PayLater_PayLater_Block_Payment_Form extends Mage_Core_Block_Template implements PayLater_PayLater_Core_Interface, PayLater_PayLater_Core_ShowableInterface, PayLater_PayLater_Core_TypeableInterface
+class PayLater_PayLater_Block_Payment_Form extends Mage_Core_Block_Template implements PayLater_PayLater_Core_Interface, PayLater_PayLater_Core_ShowableInterface, PayLater_PayLater_Core_TypeableInterface, PayLater_PayLater_Core_WidgetInterface
 {
 
-	protected function _construct()
+    protected $_widgetJson;
+    protected $_widgetConfig;
+    protected $_widgetName;
+    protected $_widgetShowBreakdown;
+    protected $_widgeParams;
+
+    protected function _construct()
 	{
 		parent::_construct();
 		$this->setTemplate(self::PAYMENT_METHOD_FORM_TEMPLATE);
@@ -86,4 +92,73 @@ class PayLater_PayLater_Block_Payment_Form extends Mage_Core_Block_Template impl
 		return $helper->canShowAtCheckout() && $helper->isAllowedCurrency();
 	}
 
+    public function hasWidgetBeenConfigured ()
+    {
+        return strlen(trim($this->getConfiguredWidgetJSON()));
+    }
+
+    public function getConfiguredWidgetJSON ()
+    {
+        $this->_widgetJson = Mage::helper('paylater')->getPayLaterConfigCheckoutConfig('widgets');
+        return $this->_widgetJson;
+    }
+
+    public function getConfiguredWidget ()
+    {
+        if (isset($this->_widgetJson)) {
+            $this->_widgetConfig = (array) json_decode($this->_widgetJson);
+        } else {
+            $this->_widgetConfig = (array) json_decode($this->getConfiguredWidgetJSON());
+        }
+        try {
+            $this->_widgetName = $this->_widgetConfig['name'];
+            foreach ($this->_widgetConfig['fields'] as $field) {
+
+                if ($field->key == 'show-breakdown') {
+                    $this->_widgetShowBreakdown = $field->value;
+                } else {
+                    $this->_widgeParams[] = "{$field->key}:{$field->value}";
+                }
+            }
+        }catch (Exception $e) {
+            /**
+             * @todo log
+             */
+        }
+
+        return $this->_widgetConfig;
+    }
+
+    public function getWidgetName()
+    {
+        return $this->_widgetName;
+    }
+
+    public function doesWidgetHaveBreakdown ()
+    {
+        return isset($this->_widgetShowBreakdown) && $this->_widgetShowBreakdown == 'true';
+    }
+
+    public function doesWidgetHaveParameters ()
+    {
+        return (isset($this->_widgeParams) && count($this->_widgeParams) > 0);
+    }
+
+    public function getWidgetShowBreakdown($type = 'string')
+    {
+        if ($type == 'string') {
+            return $this->_widgetShowBreakdown;
+        }
+        return $this->_widgetShowBreakdown == 'true' ? true : false;
+    }
+
+    public function getWidgetParamsAsString()
+    {
+        return implode(';', $this->_widgeParams);
+    }
+
+    public function getAmount ()
+    {
+        return $this->getGrandTotal();
+    }
 }
